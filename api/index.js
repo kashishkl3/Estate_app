61f1a2bc32217e91640c79871d77f23bc2694a94
Kashish Khandelwal <khandelwalkashish80@gmail.com>
2025-09-27 15:01:26 +0530

    Fix server start, static path, and deployment-ready setup

--- api/index.js
+++ api/index.js
@@ -5,51 +5,47 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-//import { SiExpertsexchange } from 'react-icons/si';
 import path from 'path';
 dotenv.config();
-import cors from 'cors';
 
-
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) =>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
     console.log(err);
-});
-const __dirname=path.resolve();
+  });
 
-const app=express();
+  const __dirname = path.resolve();
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Vite dev server URL
-  credentials: true
-}));
-
-
-
+const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing",listingRouter);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
-})
-
-app.listen(3000,() => {
-    console.log('Server is running on port 3000');
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
